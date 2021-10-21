6598eadf94a63bf59cdc42a72606a559ba946188
essekappa <mprincipe10@outlook.it>
2021-10-21 21:41:13 +0200

    Initial commit

---
+++ dizionario.py
@@ -0,0 +1,42 @@
+from tkinter import * 
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+root = Tk()
+
+root.geometry("400x400")
+def dict():
+
+     meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
+     synonym.config(text=dictionary.synonym(word.get()))
+     antonym.config(text=dictionary.antonym(word.get()))
+
+Label(root, text="Dictionary", font=("Helvetica 20 bold"), fg="Green").pack(pady=10)
+
+frame = Frame(root)
+Label(root, text="Type word", font=("Helvetica 15 bold")).pack(side=LEFT)
+word = Entry(frame, font=("Helvetica 15 bold"))
+word.pack()
+frame.pack(pady=10)
+
+frame1 = Frame(root)
+Label(frame1, text="", font=("Helvetica 10")).pack(side=LEFT)
+meaning = Label(frame1, text="", font=("Helvetica 10"))
+meaning.pack()
+frame1.pack(pady=10)
+
+frame2 = Frame(root)
+Label(frame2, text="Synonym:-", font=("Helvetica 10"))
+synonym = Label(frame2, text="", font=("Helvetica 10"))
+synonym.pack()
+frame2.pack(pady=10)
+
+
+frame3 = Frame(root)
+Label(frame3, text="Antonym:-", font=("Helvetica 10"))
+antonym = Label(frame3, text="", font=("Helvetica 10"))
+antonym.pack(side=LEFT)
+frame3.pack(pady=10)
+Button(root, text="Submit", font=("Helvetica 15 bold"), command=dict).pack()
+
+root.mainloop()
